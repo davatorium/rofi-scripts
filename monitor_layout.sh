dc614ac441177d28cb3b6fa2ffe4eaf0818d8e46
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-05-12 06:52:36 +0200

    Delete useless rofi dmenu option

--- monitor_layout.sh
+++ monitor_layout.sh
@@ -94,7 +94,7 @@ function gen_entries()
 }
 
 # Call menu
-SEL=$( gen_entries | rofi -dmenu -p "Monitor Setup:" -a 0 -no-custom  | awk '{print $1}' )
+SEL=$( gen_entries | rofi -dmenu -p "Monitor Setup:" -no-custom  | awk '{print $1}' )
 
 # Call xrandr
 $( ${COMMANDS[${SEL::-1}]} )
