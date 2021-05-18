96ebea7a5b20ceb94264194d5e5f0cb371a651d2
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-05-18 10:56:31 +0530

    Better functioning of tmux_session.sh
    Now it asks for the name of new session

--- tmux_session.sh
+++ tmux_session.sh
@@ -2,15 +2,22 @@
 
 function tmux_sessions()
 {
-    tmux list-session -F '#S'
+    # Get all existing sessions
+    tmux list-session -F '#S'|cut -d'-' -f 1
 }
+# Take the choice of session in TMUX_SESSION
+TMUX_SESSION=$( (tmux_sessions; echo "New") | rofi -dmenu -p "Select existing tmux session")
 
-TMUX_SESSION=$( (echo new; tmux_sessions) | rofi -dmenu -p "Select tmux session")
-
-if [[ x"new" = x"${TMUX_SESSION}" ]]; then
-    rofi-sensible-terminal -e tmux new-session &
+if [[ x"New" = x"${TMUX_SESSION}" ]]; then
+    # Ask for name of session
+    NAME=$(rofi -dmenu -p "Name for new session")
+    rofi-sensible-terminal -e tmux new-session -t "${NAME}" &
 elif [[ -z "${TMUX_SESSION}" ]]; then
     echo "Cancel"
 else
-    rofi-sensible-terminal -e tmux attach -t "${TMUX_SESSION}" &
+    if tmux_sessions | grep -q '${TMUX_SESSION}'; then
+	rofi-sensible-terminal -e tmux attach -t ${TMUX_SESSION} &
+    else 
+	rofi-sensible-terminal -e tmux new -t ${TMUX_SESSION} &
+    fi
 fi
